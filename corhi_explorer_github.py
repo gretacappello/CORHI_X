16c0aa937b011bac720a860e0b92058c609dd6e8
gretacappello <49552891+gretacappello@users.noreply.github.com>
2024-11-27 15:29:30 +0100

    new logo

--- corhi_explorer_github.py
+++ corhi_explorer_github.py
@@ -86,7 +86,7 @@ from matplotlib import image as mpimg
 path_local_greta = './'
 overview_path = path_local_greta
 path_to_logo = path_local_greta
-st.set_page_config(page_title="Cor-HI Explorer",page_icon=path_to_logo+"/logo_corhi.png", layout="wide")
+st.set_page_config(page_title="Cor-HI Explorer",page_icon=path_to_logo+"/corhiX_pictogram.png", layout="wide")
 
 
 def reader_txt(file_path):
@@ -352,7 +352,7 @@ with col1:
         return time2_cme_user, cme_user_r, cme_user_lat, cme_user_lon, cme_user_a, cme_user_b, cme_user_c, cme_user_id
         
     #st.header("Welcome to Cor-HI Explorer")
-    st.image(path_to_logo+"/logo_corhi.png" , width=400)
+    st.image(path_to_logo+"/logo_corhi_black_no_bkg.png" )
 
     #st.header("🔍 **Select the interval of time**")
     st.markdown("<h4 style='color: magenta;'>🔍 Select the interval of time</h4>", unsafe_allow_html=True)
