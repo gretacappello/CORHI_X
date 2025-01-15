09245c5281ca70b44d5c5d1fd86530bec8611429
gretacappello <49552891+gretacappello@users.noreply.github.com>
2025-01-15 10:59:07 +0100

    Delete config_servers.py

--- config_servers.py
+++
@@ -1,8 +0,0 @@
-data_path = '/data/aswo/data/insitu_python/'
-movie_path = '/perm/aswo/ops/elevo/movies/'
-frames_path = '/perm/aswo/ops/elevo/frames/'
-results_path = '/perm/aswo/ops/elevo/output/'
-arrival_path = '/perm/aswo/ops/elevo/arrival/'
-overview_path = '/data/aswo/eweiler/overview/'
-overview_frames_path = '/data/aswo/eweiler/overview/frames/'
-overview_movie_path = '/data/aswo/eweiler/overview/movie/'
